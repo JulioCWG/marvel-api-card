836ff45f447c2d45332338abfa1eaedba1bb3f99
Julio Cesar <castro_jc88@hotmail.com>
2017-03-11 14:00:30 -0700

    Agregado la búsqueda de personajes

--- public/app.js
+++ public/app.js
@@ -19,26 +19,82 @@ api.findSeries('avengers')
   }
   return Promise.all(promises);
 }).then(characters => {
-  return characters.filter(character => {
-    return !!character.thumbnail;
-  });
+  return characters.filter(character => !!character.thumbnail);
 }).then(characters => {
-  debugger;
-  console.log(characters);
-  //por cada carta
-  $('.Card').each((i, item) => {
+
+  ////ordenar las cartas con la funcion shuffle
+  characters = shuffle(characters);
+
+  for (let i = 0; i < 5; i++) {
+    //  let index = Math.floor(Math.random() * characters.length - 1)
+    //  let character = characters [index]
     let character = characters[i];
-    let $this = $(item);
-    let $image = $this.find('.Card-image');
-    let $description = $this.find('.Card-description');
-    let $name = $this.find('.Card-name');
-    $image.attr('src', `${character.thumbnail.path}.${character.thumbnail.extension}`);
-    $name.text(character.name);
-    $description.text(character.description);
-  });
+    drawCharacter(character);
+  }
+
+  // debugger
+  // console.log(characters);
+  // //por cada carta
+  // $('.Card').each((i, item) => {
+  //   let character = characters[i]
+  //   let $this =$(item)
+  //   let $image = $this.find('.Card-image')
+  //   let $description = $this.find('.Card-description')
+  //   let $name = $this.find('.Card-name')
+  //   $image.attr('src',`${character.thumbnail.path}.${character.thumbnail.extension}`)
+  //   $name.text(character.name)
+  //   $description.text(character.description)
   //cambiar la imagen .Card-image
   // Cambiar .Card-description
   //cambiar .card-name
 }).catch(err => {
   console.error(err);
 });
+
+$('.CharacterForm').on('submit', function (event) {
+  event.preventDefault();
+
+  let name = $(this).find('.CharacterForm-name').val();
+  api.searchCharacter(name).then(function (character) {
+    drawCharacter(character);
+  }).catch(function (reason) {
+    if (reason === 'No se encontró el personaje') $('.CharacterForm-message').text(reason);
+  });
+  //llamar a la api de marvel
+  //dibujar una carta con el personaje de la api
+  //si no regresa un personaje ->no hay personaje
+  // si regresa solo un personae -> dibujar carta
+  // si regresa mas de un personaje -> dibujar carta con el primer personaje
+});
+
+function renderCharacter(character) {
+  let attackPoints = Math.ceil(Math.random() * 500) + 500;
+  return `<div class="Card">
+    <h2 class="Card-name">${character.name}</h2>
+    <img src="${character.thumbnail.path}.${character.thumbnail.extension}" class="Card-image">
+    <div class="Card-description">${character.description}</div>
+    <div class="Card-attack" data-attack="${attackPoints}">${attackPoints} Puntos de ataque !!</div>
+  </div>`;
+}
+
+function shuffle(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    let tmp = arr[i];
+    let index = Math.floor(Math.random() * (arr.length - 1));
+    arr[i] = arr[index];
+    arr[index] = tmp;
+  }
+  return arr;
+}
+
+function drawCharacter(character) {
+  let template = renderCharacter(character);
+  let $card = $(template);
+
+  $card.on('click', function (event) {
+    let $this = $(this);
+    let attack = $this.find('.Card-attack');
+    console.log(attack.data('attack'));
+  });
+  $('.Battle-player').append($card);
+}
